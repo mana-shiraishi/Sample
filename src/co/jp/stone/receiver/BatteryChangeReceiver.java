afb406884832c54e2594176e7734c61c49c3ec90
m.shiraishi <m.shiraishi@stonesystem.co.jp>
2014-09-26 15:40:23 +0900

    文字コード修正

--- src/co/jp/stone/receiver/BatteryChangeReceiver.java
+++ src/co/jp/stone/receiver/BatteryChangeReceiver.java
@@ -1,20 +1,11 @@
 package co.jp.stone.receiver;
 
 
-import java.io.IOException;
-
-import co.jp.sample.R;
-import co.jp.stone.service.BootupService;
 import android.app.Activity;
-import android.app.WallpaperManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.BatteryManager;
-import android.util.Log;
-import android.widget.TextView;
+import co.jp.stone.service.BootupService;
 
 public class BatteryChangeReceiver extends BroadcastReceiver{
 
@@ -30,20 +21,20 @@ public class BatteryChangeReceiver extends BroadcastReceiver{
 		intent.putExtra("level", arg1.getIntExtra("level", 0));
         context.startService(intent);
 		
-		// [“dó‘Ô
+		// ï¿½[ï¿½dï¿½ï¿½ï¿½
 //		TextView tvBatteryCharge = (TextView) mActivity.findViewById(R.id.BatteryCharge);
 //        switch (intent.getIntExtra("status", 0)){
-//        case BatteryManager.BATTERY_STATUS_CHARGING://[“d’†
+//        case BatteryManager.BATTERY_STATUS_CHARGING://ï¿½[ï¿½dï¿½ï¿½
 //        	tvBatteryCharge.setText(context.getString(R.string.label_detect_charging));
 //        	break;
-//        case BatteryManager.BATTERY_STATUS_DISCHARGING://•ú“d’†
+//        case BatteryManager.BATTERY_STATUS_DISCHARGING://ï¿½ï¿½dï¿½ï¿½
 //        case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
 //        	tvBatteryCharge.setText(context.getString(R.string.label_detect_discharging));
 //        	break;
-//        case BatteryManager.BATTERY_STATUS_FULL://–ƒ^ƒ“
+//        case BatteryManager.BATTERY_STATUS_FULL://ï¿½ï¿½ï¿½^ï¿½ï¿½
 //        	tvBatteryCharge.setText(context.getString(R.string.label_detect_fullbattery));
 //        	break;
-//        case BatteryManager.BATTERY_STATUS_UNKNOWN://•s–¾
+//        case BatteryManager.BATTERY_STATUS_UNKNOWN://ï¿½sï¿½ï¿½
 //            Log.d("BatteryChange",context.getString(R.string.label_detect_status_unknown));
 //            break;
 //        }
